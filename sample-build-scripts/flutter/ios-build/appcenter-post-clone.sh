031d18e297a1535e5ba4b298bae37c40e9a0e83f
hamzakaleem430 <82136507+hamzakaleem430@users.noreply.github.com>
2021-04-08 13:09:52 +0500

    Update appcenter-post-clone.sh

--- sample-build-scripts/flutter/ios-build/appcenter-post-clone.sh
+++ sample-build-scripts/flutter/ios-build/appcenter-post-clone.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-#Place this script in project/ios/
 
-# fail if any command fails
-set -e
-# debug log
-set -x
+# Example: Clone a required repository
+git clone https://github.com/example/SomeProject
 
-cd ..
-git clone -b beta https://github.com/flutter/flutter.git
-export PATH=`pwd`/flutter/bin:$PATH
-
-flutter channel stable
-flutter doctor
-
-echo "Installed flutter to `pwd`/flutter"
-
-flutter build ios --release --no-codesign
+# Example: Install App Center CLI
+npm install -g appcenter-cli
